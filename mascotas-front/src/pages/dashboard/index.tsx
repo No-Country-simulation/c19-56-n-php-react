23837534cd471163e240e88230758ad905143ba0
krrattoss5 <krrattoss5@gmail.com>
2024-07-10 04:00:08 -0500

    (fixing branche)

--- mascotas-front/src/pages/dashboard/index.tsx
+++ mascotas-front/src/pages/dashboard/index.tsx
@@ -138,7 +138,7 @@ export default function Page() {
             </SheetContent>
           </Sheet>
         </header>
-        <CardsBackOffice />
+        <h1>Home</h1>
       </div>
     </div>
   )
