78b5b74549bdc4c868769a30320207ad4cec3f06
krrattoss5 <krrattoss5@gmail.com>
2024-07-10 02:55:32 -0500

    (add/BackofficeLayout)

--- mascotas-front/src/pages/dashboard/pet/[id].tsx
+++ mascotas-front/src/pages/dashboard/pet/[id].tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button"
-import { FilePenIcon, TrashIcon } from "../../../icons"
 import BackOffice from "@/layout/BackOffice"
 import PetDetailBackOffice from "@/components/PetDetailBackOffice"
 
