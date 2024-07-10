bdb5e545c9854f9224029c8cc5c741c9eb39b590
krrattoss5 <krrattoss5@gmail.com>
2024-07-10 04:01:02 -0500

    (fixing branche)

--- mascotas-front/src/components/CardsBackOffice.tsx
+++ mascotas-front/src/components/CardsBackOffice.tsx
@@ -1,17 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import CardBackOffice from "./CardBackOffice"
+import axios from "axios"
+import { Pet } from "@/Types"
 
 const CardsBackOffice = () => {
+  const [pets, setPets] = useState<Pet[]>([])
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/hello")
+      .then(({ data }) => setPets(data.pets))
+  }, [])
+
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+      <div className="flex items-center justify-between py-10">
+        <h1 className="text-2xl font-bold">Mascotas dsiponibles</h1>
+      </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <CardBackOffice
-          image="https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
-          name="Firulais"
-          age="2"
-          description="Firulais es un perro muy juguetón y cariñoso. Le encanta correr y
-            jugar en el parque."
-        />
+        {pets.map((pet) => (
+          <CardBackOffice
+            key={pet.id}
+            image={pet.imagen[0]}
+            name={pet.nombre}
+            age={pet.edad}
+            description={pet.descripcion}
+          />
+        ))}
       </div>
     </main>
   )
