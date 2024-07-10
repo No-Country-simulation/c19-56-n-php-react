f2bf9f8b56fa4dddcd762b983d309da42db47bbb
krrattoss5 <krrattoss5@gmail.com>
2024-07-10 03:46:01 -0500

    (branche fixed)

---
+++ mascotas-front/src/Types.d.ts
@@ -0,0 +1,14 @@
+export type Pet = {
+  id: number;
+  imagen: string[];
+  nombre: string;
+  razaID: number;
+  tamaño: string;
+  peso: string;
+  edad: string;
+  personalidad: string;
+  adopcionID: number;
+  raza: string;
+  descripcion: string;
+  categoria: string
+}
