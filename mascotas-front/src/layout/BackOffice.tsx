11c0843b726ae33a49c7421bea0881d49b485216
krrattoss5 <krrattoss5@gmail.com>
2024-07-10 01:06:57 -0500

    (add/Layout structure)

---
+++ mascotas-front/src/layout/BackOffice.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  children: React.ReactNode
+}
+
+export default function BackOffice({
+  children, // will be a page or nested layout
+}: Props) {
+  return (
+    <section>
+      <nav></nav>
+
+      {children}
+
+      <footer></footer>
+    </section>
+  )
+}
