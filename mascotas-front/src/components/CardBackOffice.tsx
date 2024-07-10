c7f7a992fdda7b228e1b0536d0867f3d278a8937
krrattoss5 <krrattoss5@gmail.com>
2024-07-09 20:50:39 -0500

    (add/components: from ui: sheets, tooltip. Created: CardBackOffice.tsx CardsBackOffice.tsx )

---
+++ mascotas-front/src/components/CardBackOffice.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent } from "@/components/ui/card"
+import Link from "next/link"
+
+interface Props {
+  image: string
+  name: string
+  age: string
+  description: string
+}
+
+const CardBackOffice = ({ image, name, age, description }: Props) => {
+  return (
+    <Card className="cursor-pointer">
+      <Link href="#">
+        <img
+          src={image}
+          alt="Mascota 1"
+          width={400}
+          height={400}
+          className="aspect-square rounded-t-lg object-cover"
+        />
+        <CardContent className="p-4">
+          <h3 className="text-xl font-bold">{name}</h3>
+          <p className="text-muted-foreground">{age} años</p>
+          <p className="text-sm leading-relaxed">{description}</p>
+        </CardContent>
+      </Link>
+    </Card>
+  )
+}
+
+export default CardBackOffice
