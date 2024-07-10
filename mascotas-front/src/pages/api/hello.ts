28a18ddd791c7888814acffbb5669130955afdf6
krrattoss5 <krrattoss5@gmail.com>
2024-07-10 02:34:21 -0500

    (add/pet mocks)

--- mascotas-front/src/pages/api/hello.ts
+++ mascotas-front/src/pages/api/hello.ts
@@ -1,13 +1,264 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { type Pet } from "@/Types.d"
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  pets: Pet[]
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+
+
+  res.status(200).json({pets: [
+    {
+      "id": 1,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Max",
+      "razaID": 101,
+      "tamaño": "Grande",
+      "peso": "30kg",
+      "edad": "5 años",
+      "personalidad": "Amigable",
+      "adopcionID": 201,
+      "raza": "Labrador Retriever",
+      "descripcion": "Un perro muy activo y cariñoso.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 2,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Bella",
+      "razaID": 102,
+      "tamaño": "Mediano",
+      "peso": "20kg",
+      "edad": "3 años",
+      "personalidad": "Juguetona",
+      "adopcionID": 202,
+      "raza": "Beagle",
+      "descripcion": "Le encanta jugar y correr.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 3,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Charlie",
+      "razaID": 103,
+      "tamaño": "Pequeño",
+      "peso": "10kg",
+      "edad": "4 años",
+      "personalidad": "Curioso",
+      "adopcionID": 203,
+      "raza": "Dachshund",
+      "descripcion": "Siempre está investigando todo a su alrededor.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 4,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Lucy",
+      "razaID": 104,
+      "tamaño": "Grande",
+      "peso": "35kg",
+      "edad": "6 años",
+      "personalidad": "Protector",
+      "adopcionID": 204,
+      "raza": "German Shepherd",
+      "descripcion": "Un perro leal y protector de su familia.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 5,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Daisy",
+      "razaID": 105,
+      "tamaño": "Mediano",
+      "peso": "18kg",
+      "edad": "2 años",
+      "personalidad": "Cariñosa",
+      "adopcionID": 205,
+      "raza": "Cocker Spaniel",
+      "descripcion": "Le encanta estar con las personas y recibir atención.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 6,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Rocky",
+      "razaID": 106,
+      "tamaño": "Grande",
+      "peso": "40kg",
+      "edad": "7 años",
+      "personalidad": "Leal",
+      "adopcionID": 206,
+      "raza": "Rottweiler",
+      "descripcion": "Muy leal y protector, ideal para familias.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 7,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Molly",
+      "razaID": 107,
+      "tamaño": "Pequeño",
+      "peso": "7kg",
+      "edad": "3 años",
+      "personalidad": "Tranquilo",
+      "adopcionID": 207,
+      "raza": "Pug",
+      "descripcion": "Un perro tranquilo que se adapta bien a los apartamentos.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 8,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Buddy",
+      "razaID": 108,
+      "tamaño": "Mediano",
+      "peso": "22kg",
+      "edad": "4 años",
+      "personalidad": "Energético",
+      "adopcionID": 208,
+      "raza": "Border Collie",
+      "descripcion": "Muy energético y necesita mucho ejercicio.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 9,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Lola",
+      "razaID": 109,
+      "tamaño": "Pequeño",
+      "peso": "8kg",
+      "edad": "5 años",
+      "personalidad": "Sociable",
+      "adopcionID": 209,
+      "raza": "Shih Tzu",
+      "descripcion": "Le encanta socializar con otros perros y personas.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 10,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Zoe",
+      "razaID": 110,
+      "tamaño": "Mediano",
+      "peso": "25kg",
+      "edad": "3 años",
+      "personalidad": "Inteligente",
+      "adopcionID": 210,
+      "raza": "Australian Shepherd",
+      "descripcion": "Muy inteligente y fácil de entrenar.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 11,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Bailey",
+      "razaID": 111,
+      "tamaño": "Grande",
+      "peso": "32kg",
+      "edad": "6 años",
+      "personalidad": "Amigable",
+      "adopcionID": 211,
+      "raza": "Golden Retriever",
+      "descripcion": "Muy amigable y le encanta estar con las personas.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 12,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Sadie",
+      "razaID": 112,
+      "tamaño": "Mediano",
+      "peso": "23kg",
+      "edad": "4 años",
+      "personalidad": "Juguetona",
+      "adopcionID": 212,
+      "raza": "Bulldog",
+      "descripcion": "Le encanta jugar y es muy cariñosa.",
+      "categoria": "Canes"
+    },
+    {
+      "id": 13,
+      "imagen": [
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg",
+        "https://elcomercio.pe/resizer/kdpOnVc-NEda1kybtWrNus0oQ6I=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/M36IGMLO7NGLHNM7222HJHMQCA.jpg"
+      ],
+      "nombre": "Oliver",
+      "razaID": 113,
+      "tamaño": "Grande",
+      "peso": "34kg",
+      "edad": "5 años",
+      "personalidad": "Protector",
+      "adopcionID": 213,
+      "raza": "Doberman",
+      "descripcion": "Un perro muy protector y leal.",
+      "categoria": "Canes"
+    },
+  ]});
 }
