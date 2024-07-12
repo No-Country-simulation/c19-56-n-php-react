1fd4998d76873f4ab524c8d7f416dc3f160e9c23
krrattoss5 <krrattoss5@gmail.com>
2024-07-12 15:26:31 -0500

    (add: PaginationComponent: Componente sin logica aún )

--- mascotas-front/src/components/CardsBackOffice.tsx
+++ mascotas-front/src/components/CardsBackOffice.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import CardBackOffice from "./CardBackOffice"
 import axios from "axios"
 import { Pet } from "@/Types"
+import Pagination from "./PaginationComponent"
 
 const CardsBackOffice = () => {
   const [pets, setPets] = useState<Pet[]>([])
@@ -15,7 +16,7 @@ const CardsBackOffice = () => {
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
       <div className="flex items-center justify-between py-10">
-        <h1 className="text-2xl font-bold">Mascotas dsiponibles</h1>
+        <h1 className="text-4xl font-bold">Mascotas dsiponibles</h1>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {pets.map((pet) => (
@@ -28,6 +29,7 @@ const CardsBackOffice = () => {
           />
         ))}
       </div>
+      <Pagination />
     </main>
   )
 }
---
+++ mascotas-front/src/components/PaginationComponent.tsx
@@ -0,0 +1,38 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationLink,
+  PaginationNext,
+} from "@/components/ui/pagination"
+
+export default function PaginationComponent() {
+  return (
+    <div className="w-full max-w-4xl mx-auto py-8">
+      <div className="flex justify-center mt-8">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                1
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </div>
+  )
+}
