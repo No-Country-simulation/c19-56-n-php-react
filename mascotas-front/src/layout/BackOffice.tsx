e7ef416f1cde167ef215baa53bf9048da040a5a9
krrattoss5 <krrattoss5@gmail.com>
2024-07-12 11:16:09 -0500

    (refact: se comonentizo el item de navegacion para no repetir estilizado de tailwind. )

--- mascotas-front/src/layout/BackOffice.tsx
+++ mascotas-front/src/layout/BackOffice.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar"
 
 interface Props {
   children: React.ReactNode;
@@ -7,6 +8,7 @@ export default function BackOffice({ children }: Props) {
   return (
     <section className="flex h-screen">
       <nav>
+        <Sidebar />
       </nav>
       <main className="flex-1 overflow-y-auto">
         {children}
