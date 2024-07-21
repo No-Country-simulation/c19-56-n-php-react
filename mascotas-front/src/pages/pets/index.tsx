770d0fad39950dde30e28eadbea96b4ffba6b2ba
krrattoss5 <krrattoss5@gmail.com>
2024-07-20 20:20:18 -0500

    (add/page: pets & pets/id )

---
+++ mascotas-front/src/pages/pets/index.tsx
@@ -0,0 +1,12 @@
+import ClientLayout from '@/layout/ClientLayout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <ClientLayout>
+      <h1>mascota uno</h1>
+    </ClientLayout>
+  )
+}
+
+export default index
