04f8ee32884501bfe073e69e1b48d92ad3d7d7dd
krrattoss5 <krrattoss5@gmail.com>
2024-07-22 16:03:41 -0500

    (register connect)

--- mascotas-front/src/backend/endpoints.ts
+++ mascotas-front/src/backend/endpoints.ts
@@ -20,3 +20,24 @@ export const login = async (email: string, password: string) => {
     }
   }
 };
+
+export const register = async (name: string, email: string, password: string, password_confirmation: string, role: string ) => {
+  try {
+    const response = await urlBase.post(
+      `/api/register`,
+      { name, email, password, password_confirmation, role },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    return response;
+  } catch (error: any) {
+    if (error.response && error.response.data && error.response.data.error) {
+      console.error("Error del backend:", error.response.data.error);
+      throw new Error(error.response.data.error);
+    } else {
+      console.error("Error de red o desconocido", error);
+      throw new Error(
+        "Ocurrió un error al intentar realizar la operación. Por favor, inténtalo de nuevo."
+      );
+    }
+  }
+};
