a60a279452af1ea3ca17a432cd61edcecf8bca12
krrattoss5 <krrattoss5@gmail.com>
2024-07-24 13:42:15 -0500

    (create/Page): Create backOffice page for add new pets

---
+++ mascotas-front/src/pages/dashboard/pets/create.tsx
@@ -0,0 +1,23 @@
+import CreatePetForm from "@/components/CreatePetForm"
+import BackOffice from "@/layout/BackOffice"
+import React from "react"
+
+const create = () => {
+  return (
+    <BackOffice>
+      <div
+        className="relative flex min-h-screen items-center justify-center bg-cover bg-center py-10"
+        style={{ backgroundImage: "url('/image-adopt.jpeg')" }}
+      >
+        <div className="bg-white bg-opacity-30 p-8 rounded-lg shadow-lg max-w-lg w-full">
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-[#1f9063]">
+            Formulario de Adopción
+          </h2>
+          <CreatePetForm />
+        </div>
+      </div>
+    </BackOffice>
+  )
+}
+
+export default create
